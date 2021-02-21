606a1d510d53175af2fb356d5e38a8896f958cb7
Piotr Tomczak <230025@edu.p.lodz.pl>
2021-02-21 14:33:36 +0100

    Currency viewer with access to Cinkciarz.pl for currency view, using Tkinter and BeautifulSoup

---
+++ main.py
@@ -0,0 +1,106 @@
+import requests as req
+import tkinter as tk
+from bs4 import BeautifulSoup
+
+window = tk.Tk()
+window.title("Currency Viewer")
+window.geometry("550x250")
+window.resizable(0, 0)
+
+CHF = "https://cinkciarz.pl/wymiana-walut/kursy-walut/chf/pln"
+USD = "https://cinkciarz.pl/wymiana-walut/kursy-walut/usd/pln"
+EURO = "https://cinkciarz.pl/wymiana-walut/kursy-walut/eur/pln"
+GBP = "https://cinkciarz.pl/wymiana-walut/kursy-walut/gbp/pln"
+
+chf_page = req.get(CHF)
+usd_page = req.get(USD)
+euro_page = req.get(EURO)
+gbp_page = req.get(GBP)
+
+chf = BeautifulSoup(chf_page.content, 'html.parser')
+usd = BeautifulSoup(usd_page.content, 'html.parser')
+eur = BeautifulSoup(euro_page.content, 'html.parser')
+gbp = BeautifulSoup(gbp_page.content, 'html.parser')
+
+chf_buy_course = chf.find('td', {'data-buy': 'true'})
+chf_sell_course = chf.find('td', {'data-sell': 'true'})
+
+try:
+    span_chf_buy_element = chf_buy_course.find_all('span').pop(1)
+    span_chf_sell_element = chf_sell_course.find_all('span').pop(1)
+    chf_buy_course_label_str = "Buy: " + str(span_chf_buy_element).replace("<span>", "").replace("</span>", "") + " PLN"
+    chf_sell_course_label_str = "Sell: " + str(span_chf_sell_element).replace("<span>", "").replace("</span>", "") + " PLN"
+except Exception as e:
+    chf_buy_course_label_str = "Buy:  PLN"
+    chf_sell_course_label_str = "Sell: " + "PLN"
+
+chf_label = tk.Label(window, text="CHF: ", font=12, pady=5, padx=5)
+chf_buy_label = tk.Label(window, text=chf_buy_course_label_str, font=12, padx=5)
+chf_sell_label = tk.Label(window, text=chf_sell_course_label_str, font=12, padx=5)
+chf_label.grid(row=0, column=0)
+chf_buy_label.grid(row=1, column=1)
+chf_sell_label.grid(row=2, column=1)
+
+usd_buy_course = usd.find('td', {'data-buy': 'true'})
+usd_sell_course = usd.find('td', {'data-sell': 'true'})
+
+try:
+    span_usd_buy_element = usd_buy_course.find_all('span').pop(1)
+    span_usd_sell_element = usd_sell_course.find_all('span').pop(1)
+    usd_buy_course_label_str = "Buy: " + str(span_usd_buy_element).replace("<span>", "").replace("</span>", "") + " PLN"
+    usd_sell_course_label_str = "Sell: " + str(span_usd_sell_element).replace("<span>", "").replace("</span>", "") + " PLN"
+except Exception as e:
+    usd_buy_course_label_str = "Buy:  PLN"
+    usd_sell_course_label_str = "Sell:  PLN"
+
+usd_label = tk.Label(window, text="USD:", font=16, pady=5, padx=5)
+usd_buy_label = tk.Label(window, text=usd_buy_course_label_str, font=12, padx=5)
+usd_sell_label = tk.Label(window, text=usd_sell_course_label_str, font=12, padx=5)
+usd_label.grid(row=3, column=0)
+usd_buy_label.grid(row=4, column=1)
+usd_sell_label.grid(row=5, column=1)
+
+euro_buy_course = eur.find('td', {'data-buy': 'true'})
+euro_sell_course = eur.find('td', {'data-sell': 'true'})
+
+try:
+    span_euro_buy_element = euro_buy_course.find_all('span').pop(1)
+    span_euro_sell_element = euro_sell_course.find_all('span').pop(1)
+    euro_buy_course_label_str = "Buy: " + str(span_euro_buy_element).replace("<span>", "").replace("</span>", "") + " PLN"
+    euro_sell_course_label_str = "Sell: " + str(span_euro_sell_element).replace("<span>", "").replace("</span>", "") + " PLN"
+except Exception as e:
+    euro_buy_course_label_str = "Buy: PLN"
+    euro_sell_course_label_str = "Sell: PLN"
+
+euro_label = tk.Label(window, text="EUR: ", font=16, pady=5, padx=10)
+euro_buy_label = tk.Label(window, text=euro_buy_course_label_str, font=12, padx=5)
+euro_sell_label = tk.Label(window, text=euro_sell_course_label_str, font=12, padx=5)
+euro_label.grid(row=0, column=5)
+euro_buy_label.grid(row=1, column=6)
+euro_sell_label.grid(row=2, column=6)
+
+gbp_buy_course = gbp.find('td', {'data-buy': 'true'})
+gbp_sell_course = gbp.find('td', {'data-sell': 'true'})
+
+try:
+    span_gbp_buy_element = gbp_buy_course.find_all('span').pop(1)
+    span_gbp_sell_element = gbp_sell_course.find_all('span').pop(1)
+    gbp_buy_course_label_str = "Buy: " + str(span_gbp_buy_element).replace("<span>", "").replace("</span>", "") + " PLN"
+    gbp_sell_course_label_str = "Sell: " + str(span_gbp_sell_element).replace("<span>", "").replace("</span>",
+                                                                                                    "") + " PLN"
+except Exception as e:
+    gbp_buy_course_label_str = "Buy: PLN"
+    gbp_sell_course_label_str = "Sell: PLN"
+
+gbp_label = tk.Label(window, text="GBP: ", font=16, pady=5, padx=10)
+gbp_buy_label = tk.Label(window, text=gbp_buy_course_label_str, font=12, padx=5)
+gbp_sell_label = tk.Label(window, text=gbp_sell_course_label_str, font=12, padx=5)
+gbp_label.grid(row=3, column=5)
+gbp_buy_label.grid(row=4, column=6)
+gbp_sell_label.grid(row=5, column=6)
+
+exit_button = tk.Button(window, text="Exit", command=window.destroy)
+exit_button.grid(row=6, column=4)
+
+if __name__ == "__main__":
+    window.mainloop()
